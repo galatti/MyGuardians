06c16aa391673a5f156d91529cc57968fe87bda9
galatti <galatti@hotmail.com>
2015-03-04 17:49:41 -0300

    add getMembershipId

--- app/src/main/java/com/example/galatti/myguardians/AsyncTaskParseJson.java
+++ app/src/main/java/com/example/galatti/myguardians/AsyncTaskParseJson.java
@@ -8,22 +8,60 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by rgalatti on 04/03/2015.
+ * Created by Rodrigo Galatti on 04/03/2015.
  */
 public class AsyncTaskParseJson extends AsyncTask<String, String, String> {
 
     final String TAG = "AsyncTaskParseJson.java";
 
-    // set your json string url here
-    String yourJsonStringUrl = "http://www.bungie.net/Platform/Destiny/2/Account/4611686018428989961";
-
-    JSONArray dataJsonArr = null;
 
     @Override
     protected void onPreExecute() {}
 
     @Override
     protected String doInBackground(String... arg0) {
+
+        String membershipId = getMembershipId("sandman_br");
+
+        getGuardians(membershipId);
+
+        return null;
+    }
+
+    private String getMembershipId(String id) {
+
+        String membershipId = null;
+
+        String yourJsonStringUrl = "http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/2/" + id;
+
+        JSONArray dataJsonArr;
+
+        try {
+
+            // instantiate our json parser
+            JsonParser jParser = new JsonParser();
+
+            // get json string from url
+            JSONObject json = jParser.getJSONFromUrl(yourJsonStringUrl);
+
+            dataJsonArr = json.getJSONArray("Response");
+            JSONObject member = dataJsonArr.getJSONObject(0);
+            membershipId = member.getString("membershipId");
+
+            Log.e(TAG, "membershipId: " + membershipId);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return membershipId;
+    }
+
+    private void getGuardians(String membershipId) {
+
+        String yourJsonStringUrl = "http://www.bungie.net/Platform/Destiny/2/Account/" + membershipId;
+
+        JSONArray dataJsonArr;
 
         try {
 
@@ -54,8 +92,6 @@ public class AsyncTaskParseJson extends AsyncTask<String, String, String> {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-        return null;
     }
 
     @Override
